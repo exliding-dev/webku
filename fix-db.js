7a5d4637ddb7efedbd62d3de6abe74f0614c5c16
sasongkodev <sasongkodev@gmail.com>
2026-04-24 11:20:48 +0700

    add45

--- fix-db.js
+++ fix-db.js
@@ -1,13 +1,20 @@
-import { Client } from 'pg';
+import { Client } from 'pg'
 const client = new Client({
-  connectionString: 'postgresql://postgres.bznatjpmsapialqfjmoh:Exliding@11@aws-1-ap-southeast-2.pooler.supabase.com:5432/postgres'
-});
-client.connect().then(() => {
-  return client.query("ALTER TABLE portfolios DROP COLUMN IF EXISTS image; ALTER TABLE portfolios ADD COLUMN IF NOT EXISTS image_id integer;");
-}).then(() => {
-  console.log("Database updated");
-  client.end();
-}).catch(err => {
-  console.error("Error:", err);
-  client.end();
-});
+  connectionString:
+    'postgresql://postgres.bznatjpmsapialqfjmoh:Exliding@11@aws-1-ap-southeast-2.pooler.supabase.com:5432/postgres'
+})
+client
+  .connect()
+  .then(() => {
+    return client.query(
+      'ALTER TABLE portfolios DROP COLUMN IF EXISTS image; ALTER TABLE portfolios ADD COLUMN IF NOT EXISTS image_id integer;'
+    )
+  })
+  .then(() => {
+    console.log('Database updated')
+    client.end()
+  })
+  .catch((err) => {
+    console.error('Error:', err)
+    client.end()
+  })
